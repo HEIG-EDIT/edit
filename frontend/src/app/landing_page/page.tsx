3141b520858b0d3cd1d9118a1bec1336496b9950
Saskya Panchaud <saskya.panchaud@heig-vd.ch>
2025-07-17 21:30:29 +0200

    [feature] Create QuickSummary component (second block of mockup)

--- frontend/src/app/landing_page/page.tsx
+++ frontend/src/app/landing_page/page.tsx
@@ -1,10 +1,14 @@
 import { TitleBlock } from "../../components/landing_page/titleBlock";
+import { QuickSummary } from "../../components/landing_page/quickSummary";
 
 export default function LandingPage() {
   return (
     <main className="bg-gray-900 min-h-screen pt-1">
       <div className="sticky top-0">
         <TitleBlock />
+      </div>
+      <div className="mt-24 ml-12">
+        <QuickSummary />
       </div>
     </main>
   );
