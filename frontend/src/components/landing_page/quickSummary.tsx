700ae3c15c16851dbccb6fa14bd9220d7f60f3cf
Saskya Panchaud <saskya.panchaud@heig-vd.ch>
2025-07-21 15:36:32 +0200

    [feature] Make page responsive

--- frontend/src/components/landing_page/quickSummary.tsx
+++ frontend/src/components/landing_page/quickSummary.tsx
@@ -1,14 +1,15 @@
 import { StartButton } from "./startButton";
+import { OutsideCard } from "../outsideCard";
 
 export const QuickSummary = () => {
   return (
-    <div className="bg-gray-800 rounded-lg w-[600px] overflow-hidden">
-      <p className="text-violet-50 font-bold text-xl p-4">
+    <OutsideCard>
+      <p className="text-violet-50 font-bold text-xl mb-4">
         Easy Digital Image Toolkit
       </p>
-      <div className="bg-gray-600 rounded-lg m-6">
-        <div className="bg-violet-300 rounded-lg">
-          <p className="text-grey-800 p-4 font-semibold">
+      <div className="bg-gray-600 rounded-lg mb-4">
+        <div className="bg-violet-300 rounded-lg p-4">
+          <p className="text-grey-800 font-semibold">
             Design, modify, combine and improve images right in your browser
           </p>
         </div>
@@ -17,13 +18,14 @@ export const QuickSummary = () => {
           not) illustrations in minutes. Store projects on the cloud and work
           from anywhere.
           <br />
+          <br />
           Easily import images in various formats into multiple layers and
           export compositions with a configurable size.
         </p>
       </div>
-      <div className="pb-4 flex justify-center">
+      <div className="flex justify-center">
         <StartButton />
       </div>
-    </div>
+    </OutsideCard>
   );
 };
--- frontend/src/components/landing_page/uiPreview.tsx
+++ frontend/src/components/landing_page/uiPreview.tsx
@@ -1,62 +1,66 @@
 import Image from "next/image";
+import { OutsideCard } from "../outsideCard";
 
 export const UiPreview = () => {
   return (
-    <div className=" bg-gray-800 rounded-lg w-[1000px] overflow-hidden">
-      <div className="m-6">
+    <OutsideCard>
+      <div className="mb-4 overflow-hidden">
         <Image
           src="/landing_page/full_ui.jpg"
           alt="Full UI overview"
-          width={1000}
-          height={1000}
-          className="rounded-lg border-3 border-violet-300"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="w-full h-auto rounded-lg border-4 border-violet-300"
         />
       </div>
-      <div>
-        <p className="text-violet-50 font-bold text-2xl flex justify-center pb-6">
-          Feature rich software with a pleasant interface
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <div className="bg-gray-600 rounded-lg w-3/5 flex flex-row items-center justify-between mb-7 ml-12">
-          <div className=" bg-gray-800 rounded-lg basis-1/2 m-2">
+      <p className="text-violet-50 font-bold text-2xl flex justify-center mb-4">
+        Feature rich software with a pleasant interface
+      </p>
+      <div className="flex flex-col lg:flex-row gap-4">
+        <div className="bg-gray-600 rounded-lg p-4 flex flex-col lg:flex-row lg:flex-1 items-center">
+          <div className=" bg-gray-800 rounded-lg overflow-hidden w-full lg:w-1/2 max-w-xs mx-auto lg:mx-0">
             <Image
               src="/landing_page/layers_panel.jpg"
               alt="Layers panel overview"
-              width={1000}
-              height={1000}
-              className="rounded-lg"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-auto rounded-lg"
             />
           </div>
-          <p className="text-violet-50 text-xs basis-1/2 m-3">
-            Compose images to make complex scenes with layers.
-            <br />
-            <br />
-            Easily stack images and adjust opacity or fully hide layers.
-          </p>
-        </div>
-        <div className="bg-gray-600 rounded-lg w-2/5 flex flex-col justify-between mb-7 mr-12">
-          <div className=" basis-1/2 m-2">
-            <div className=" bg-gray-800 rounded-lg p-1 mt-2">
-              <Image
-                src="/landing_page/toolbar.jpg"
-                alt="Toolbar overview"
-                width={1000}
-                height={1000}
-                className="rounded-lg"
-              />
-            </div>
+          <div className="w-full lg:w-1/2 p-4">
+            <p className="text-violet-50 text-xs text-center">
+              Compose images to make complex scenes with layers.
+              <br />
+              <br />
+              Easily stack images and adjust opacity or fully hide layers.
+            </p>
           </div>
-          <p className="text-violet-50 text-xs basis-1/2 m-5">
-            Comprehensive set of tools for easy manipulation and editing of
-            images.
-            <br />
-            <br />
-            Move, crop, resize, draw and add filters to make the visuals of your
-            dreams.
-          </p>
+        </div>
+        <div className="bg-gray-600 rounded-lg p-4 flex flex-col">
+          <div className=" bg-gray-800 rounded-lg overflow-hidden">
+            <Image
+              src="/landing_page/toolbar.jpg"
+              alt="Toolbar overview"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-auto rounded-lg p-2"
+            />
+          </div>
+          <div className="w-full p-4">
+            <p className="text-violet-50 text-xs text-center mt-12">
+              Comprehensive set of tools for easy manipulation and editing of
+              images.
+              <br />
+              <br />
+              Move, crop, resize, draw and add filters to make the visuals of
+              your dreams.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </OutsideCard>
   );
 };
