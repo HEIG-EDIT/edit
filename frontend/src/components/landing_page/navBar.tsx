bf9c105898385eb3be5b06fcbc0202f487b1f3c4
Alessio Giuliano <alessio.giuliano@proton.me>
2025-07-23 18:58:28 +0200

    Add undo/redo section + improve style a bit more

--- frontend/src/components/landing_page/navBar.tsx
+++ frontend/src/components/landing_page/navBar.tsx
@@ -2,7 +2,7 @@ import { StartButton } from "./startButton";
 
 export const NavBar = () => {
   return (
-    <div className="bg-gray-800 rounded-xl border-2 my-10 mx-50 min-w-67 border-violet-300 min-h-16 px-4 flex items-center justify-between">
+    <div className="bg-gray-800 rounded-xl border-2 md:mx-10 lg:mx-50 min-w-67 border-violet-300 min-h-16 px-4 flex items-center justify-between">
       <p className="text-violet-50 text-2xl">EDIT</p>
       <StartButton />
     </div>
--- frontend/src/components/landing_page/projectSelection.tsx
+++ frontend/src/components/landing_page/projectSelection.tsx
@@ -1,32 +1,36 @@
 import Image from "next/image";
 import { OutsideCard } from "../outsideCard";
+import { TextCard } from "./textCard";
 
 export const ProjectSelection = () => {
   return (
     <OutsideCard>
-      <p className="text-violet-50 font-bold text-2xl flex justify-center mb-4">
+      <p className="text-violet-50 font-bold text-2xl flex justify-center mb-8">
         Store your projects securely and work from anywhere
       </p>
-      <div className="overflow-hidden mb-4">
+      <div className="overflow-hidden mb-8">
         <Image
           src="/landing_page/project_selection.jpg"
           alt="Project selection overview"
           width={0}
           height={0}
           sizes="100vw"
-          className="w-full h-auto rounded-lg border-4 border-violet-300"
+          className="w-full h-auto rounded-2xl border-4 border-violet-300"
         />
       </div>
-      <div className="bg-gray-600 rounded-lg mx-6 p-4">
-        <div className="flex flex-col lg:flex-row lg:divide-x lg:divide-violet-50 divide-y lg:divide-y-0 divide-violet-50">
-          <p className="text-violet-50 text-sm text-center lg:pr-6 pb-4 lg:pb-0 lg:flex-1">
+      <div className="bg-gray-600 rounded-2xl mx-6 p-4">
+        <div className="content-around flex flex-col lg:flex-row lg:divide-x lg:divide-violet-50 divide-y lg:divide-y-0 divide-violet-50">
+        <TextCard>
             Create new projects anytime. Change the sorting behavior to easily
             find your work.
-          </p>
-          <p className="text-violet-50 text-sm text-center lg:pl-6 pt-4 lg:pt-0 lg:flex-1">
+
             Save your work and continue later. Connect to your account to access
             your projects on any computer.
-          </p>
+        </TextCard>
+        <TextCard>
+            Share projects with your team for easy collaboration. Give edition
+            access to all authorized users.
+        </TextCard>
         </div>
       </div>
     </OutsideCard>
