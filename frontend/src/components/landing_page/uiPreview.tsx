53dba0e2abf7895475b739c75b8d56b9ef7550a4
Alessio Giuliano <alessio.giuliano@proton.me>
2025-07-23 18:58:28 +0200

    Add undo/redo section + improve style a bit more

--- frontend/src/components/landing_page/uiPreview.tsx
+++ frontend/src/components/landing_page/uiPreview.tsx
@@ -5,6 +5,7 @@ import { TextCard } from "./textCard";
 export const UiPreview = () => {
   return (
     <OutsideCard>
+
       <div className="mb-4 overflow-hidden">
         <Image
           src="/landing_page/full_ui.jpg"
@@ -15,11 +16,13 @@ export const UiPreview = () => {
           className="w-full h-auto rounded-2xl border-4 border-violet-300"
         />
       </div>
+
       <p className="text-violet-50 font-bold text-2xl flex justify-center my-12">
         Feature rich software with a pleasant interface
       </p>
-      <div className="flex flex-col lg:flex-row gap-4">
-        <div className="bg-gray-600 rounded-2xl p-4 flex flex-col lg:flex-row lg:flex-1 items-center">
+
+      <div className="grid h-xl grid-flow-row grid-rows-3 grid-cols-1 lg:grid-rows-2 lg:grid-cols-8 gap-4">
+        <div className="bg-gray-600 rounded-2xl flex flex-col lg:flex-row lg:flex-1 lg:row-span-7 lg:col-span-5 p-4 items-center">
           <div className=" bg-gray-800 rounded-2xl overflow-hidden w-full max-w-xs mx-auto lg:mx-0">
             <Image
               src="/landing_page/layers_panel.jpg"
@@ -30,7 +33,7 @@ export const UiPreview = () => {
               className="w-full h-auto rounded-2xl"
             />
           </div>
-          <div className="text-xl content-center w-full px-4 ">
+          <div className="text-xl content-center w-full px-4">
             <TextCard>
               Compose images to make complex scenes with layers.
             </TextCard>
@@ -39,7 +42,8 @@ export const UiPreview = () => {
             </TextCard>
           </div>
         </div>
-        <div className="bg-gray-600 rounded-2xl p-4 flex flex-col">
+
+        <div className="bg-gray-600 rounded-2xl p-4 lg:row-span-7 lg:col-span-3">
           <div className=" bg-gray-800 rounded-2xl mb-2 overflow-hidden">
             <Image
               src="/landing_page/toolbar.jpg"
@@ -51,17 +55,35 @@ export const UiPreview = () => {
             />
           </div>
           <div className="w-full max-w-lg p-4">
-          <TextCard>
+            <TextCard>
               Comprehensive set of tools for easy manipulation and editing of
               images.
-          </TextCard>
-          <TextCard>
+            </TextCard>
+            <TextCard>
               Move, crop, resize, draw and add filters to make the visuals of
               your dreams.
-          </TextCard>
+            </TextCard>
+          </div>
+        </div>
+
+        <div className="bg-gray-600 rounded-2xl p-4 lg:row-span-1 lg:col-span-8 ">
+          <div className="md:row-start-2 bg-gray-600 flex md:flex-col lg:flex-row items-center">
+            <div className="text-center p-4">
+              <TextCard>
+                Easily fix mistakes or undo bad ideas. Securely save projects with the click of a button.
+              </TextCard>
+            </div>
+            <Image
+              src="/landing_page/undo_redo.png"
+              alt="Toolbar overview"
+              width={418}
+              height={144}
+              className="h-full max-w-[200px] w-auto rounded-2xl p-2 "
+            />
           </div>
         </div>
       </div>
+
     </OutsideCard>
   );
 };
