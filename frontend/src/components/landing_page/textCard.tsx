571367471c35f2bdda259f4fe3655ab378a21fef
Alessio Giuliano <alessio.giuliano@proton.me>
2025-07-24 19:49:47 +0200

    Try fixing more things

--- frontend/src/components/landing_page/textCard.tsx
+++ frontend/src/components/landing_page/textCard.tsx
@@ -1,6 +1,6 @@
 export const TextCard = ({ children }) => {
   return (
-    <div className="md:text-xl text-base font-regular text-violet-50 p-3 md:p-6">
+    <div className="lg:text-lg xl:text-xl text-base font-regular text-violet-50 p-3 md:p-6">
       <p className="text-left">
         {children}
       </p>
