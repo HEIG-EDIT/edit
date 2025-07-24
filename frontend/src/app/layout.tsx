c962efcb64deecac35b00799c5d24d1a3d6555da
Saskya Panchaud <saskya.panchaud@heig-vd.ch>
2025-07-24 18:36:33 +0200

    [feature] Clean use of tailwind and apply Ubuntu font on all app

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Ubuntu } from "next/font/google";
 import "./globals.css";
-import Link from "next/link"
-import Image from "next/image"
+import Link from "next/link";
+import Image from "next/image";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const ubuntu = Ubuntu({
+  variable: "--font-ubuntu",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -30,27 +27,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen overflow-x-hidden`}
-      >
-      <nav className="bg-gray-700 shadow-md py-2 px-4 flex flex-col items-start gap-2 sm:flex-row sm:items-center sm:justify-between">
-          <Link href="/" className="w-10 h-10 relative">
-              <Image
-                  src="globe.svg"
-                  alt="EDIT"
-                  fill
-                  className="navLogo object-contain"
-              />
+    <html lang="en" className={`${ubuntu.variable}`}>
+      <body className="antialiased min-h-screen overflow-x-hidden">
+        <nav className="bg-gray-900 shadow-md py-2 px-4 flex flex-col items-start gap-2 sm:flex-row sm:items-center sm:justify-between">
+          <Link href="/" className="w-10 h-10 relative hidden">
+            <Image
+              src="globe.svg"
+              alt="EDIT"
+              fill
+              className="navLogo object-contain"
+            />
           </Link>
           <div className="space-x-4 mt-2 sm:mt-0">
-
+            {/* WIP : en prevision d'autres liens */}
           </div>
-      </nav>
+        </nav>
+
         {children}
-      <footer className="bg-gray-700 text-center text-sm p-4 text-gray-600">
+
+        <footer className="bg-gray-700 text-center text-sm p-4 text-gray-600">
           &copy; 2025 EDIT. All rights reserved.
-      </footer>
+        </footer>
       </body>
     </html>
   );
