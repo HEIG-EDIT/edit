977a7a6ab113840b2fb5f3b3adf07dfecdd3e6f4
gitBugD <114420895+gitBugD@users.noreply.github.com>
2025-07-29 11:00:16 +0200

    Update backend/src/app.module.ts
    Co-authored-by: Alessio Giuliano <117195850+AlessioGiuliano@users.noreply.github.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { StorageModule } from './storage/storage.module';
-//import { ConfigModule } from './config/config.module';
 import { TokensService } from './tokens/tokens.service';
 import { EmailService } from './email/email.service';
 
