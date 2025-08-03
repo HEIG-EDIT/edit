64584b3f98aff2fe17c12427d716a57a8b135967
Saskya Panchaud <saskya.panchaud@heig-vd.ch>
2025-08-03 12:32:51 +0200

    Update packages + remove useless image + update text to follow mockup

--- frontend/src/components/outsideCard.tsx
+++ frontend/src/components/outsideCard.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+
 export const OutsideCard = ({ children }: { children: ReactNode }) => {
   return <div className="bg-gray-800 rounded-2xl p-8">{children}</div>;
 };
