c48d9772a38daa0b5d88873d8bc12e7274c6e662
Saskya Panchaud <saskya.panchaud@heig-vd.ch>
2025-08-03 15:32:48 +0200

    [fix] Make UI more user-friendly

--- frontend/src/components/landing_page/projectSelection.tsx
+++ frontend/src/components/landing_page/projectSelection.tsx
@@ -8,17 +8,17 @@ export const ProjectSelection = () => {
       <p className="text-violet-50 font-bold text-2xl flex justify-center mb-8">
         Store your projects securely and work from anywhere
       </p>
-      <div className="overflow-hidden mb-8">
-        <Image
-          src="/landing_page/project_selection.jpg"
-          alt="Project selection overview"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="w-full h-auto rounded-2xl border-4 border-violet-300"
-        />
-      </div>
       <div className="bg-gray-600 rounded-2xl mx-6 p-4">
+        <div className="overflow-hidden mb-8">
+          <Image
+            src="/landing_page/project_selection.jpg"
+            alt="Project selection overview"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto rounded-2xl border-4 border-violet-300"
+          />
+        </div>
         <div className="content-around flex flex-col lg:flex-row lg:divide-x lg:divide-violet-50 divide-y lg:divide-y-0 divide-violet-50">
           <TextCard>
             Create new projects anytime. Change the sorting behavior to easily
--- frontend/src/components/landing_page/uiPreview.tsx
+++ frontend/src/components/landing_page/uiPreview.tsx
@@ -5,7 +5,6 @@ import { TextCard } from "./textCard";
 export const UiPreview = () => {
   return (
     <OutsideCard>
-
       <div className="mb-4 overflow-hidden">
         <Image
           src="/landing_page/full_ui.jpg"
@@ -25,12 +24,12 @@ export const UiPreview = () => {
         <div className="bg-gray-600 rounded-2xl flex row-span-4 flex-col lg:flex-row lg:flex-1 lg:row-span-7 lg:col-span-5 p-4 items-center">
           <div className=" bg-gray-800 rounded-2xl overflow-hidden w-full max-w-xs mx-auto lg:mx-0">
             <Image
-              src="/landing_page/layers_panel.jpg"
+              src="/landing_page/layers_panel.png"
               alt="Layers panel overview"
               width={0}
               height={0}
               sizes="100vw"
-              className="w-full h-auto rounded-2xl"
+              className="w-full h-auto rounded-2xl border-4 border-violet-300"
             />
           </div>
           <div className="text-xl content-center w-full px-4">
@@ -43,15 +42,15 @@ export const UiPreview = () => {
           </div>
         </div>
 
-        <div className="bg-gray-600  rounded-2xl p-4 row-span-3 lg:row-span-7 lg:col-span-3">
+        <div className="bg-gray-600 rounded-2xl p-4 row-span-3 lg:row-span-7 lg:col-span-3">
           <div className=" bg-gray-800 rounded-2xl mb-2 overflow-hidden">
             <Image
-              src="/landing_page/toolbar.jpg"
+              src="/landing_page/toolbar.png"
               alt="Toolbar overview"
               width={0}
               height={0}
               sizes="100vw"
-              className="w-full h-auto rounded-2xl p-2"
+              className="w-full h-auto rounded-2xl border-4 border-violet-300"
             />
           </div>
           <div className="w-full max-w-lg row-span-1 p-4">
@@ -66,24 +65,26 @@ export const UiPreview = () => {
           </div>
         </div>
 
-        <div className="bg-gray-600 rounded-2xl  p-4 lg:row-span-1 lg:col-span-8 ">
+        <div className="bg-gray-600 rounded-2xl p-4 lg:row-span-1 lg:col-span-8">
           <div className="md:row-start-2 bg-gray-600 flex md:flex-col lg:flex-row items-center">
             <div className="text-center overflow-hidden">
               <TextCard>
-                Easily fix mistakes or undo bad ideas. Securely save projects with the click of a button.
+                Easily fix mistakes or undo bad ideas. Securely save projects
+                with the click of a button.
               </TextCard>
             </div>
-            <Image
-              src="/landing_page/undo_redo.png"
-              alt="Toolbar overview"
-              width={418}
-              height={144}
-              className="h-full max-w-3xs w-auto rounded-2xl p-2 "
-            />
+            <div className="bg-gray-800 rounded-2xl overflow-hidden">
+              <Image
+                src="/landing_page/undo_redo.png"
+                alt="Undo / Redo overview"
+                width={418}
+                height={144}
+                className="h-full max-w-3xs w-auto rounded-2xl border-4 border-violet-300"
+              />
+            </div>
           </div>
         </div>
       </div>
-
     </OutsideCard>
   );
 };
