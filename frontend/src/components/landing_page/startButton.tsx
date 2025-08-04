d5bba81a52851f153f6f8c6dd79bc82108d31c9c
Alessio Giuliano <alessio.giuliano@proton.me>
2025-08-04 10:42:41 +0200

    Fix start button

--- frontend/src/components/landing_page/startButton.tsx
+++ frontend/src/components/landing_page/startButton.tsx
@@ -6,7 +6,7 @@ export const StartButton = () => {
   const router = useRouter();
   return (
     <button
-      className="bg-violet-500 hover:cursor-pointer hover:bg-violet-300 hover:text-gray-800 text-violet-50 text-xl font-extrabold rounded-full px-4 py-2 border-3 hover:border-violet-500 border-violet-50"
+      className="bg-violet-500 hover:cursor-pointer hover:bg-violet-400  text-violet-50 text-xl font-extrabold rounded-full px-4 py-2 border-3  border-violet-50"
       onClick={() => router.push("/")}
     >
       Start EDITing
