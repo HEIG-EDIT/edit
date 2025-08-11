832e70a4f42949961b6af04a1039e3fa931a3e14
Alessio Giuliano <alessio.giuliano@proton.me>
2025-08-11 16:36:15 +0200

    Add initialization of CircularBuffer + add undo/redo hook

--- frontend/src/components/editor/utils/CircularBuffer.ts
+++ frontend/src/components/editor/utils/CircularBuffer.ts
@@ -1,29 +1,29 @@
 export class CircularBuffer<T> {
   private elements: Array<T>;
   /// The actual length of the buffer, including the overwritten elements
-  private length;
+  private _length;
 
   /// Get the lowest index still stored in the buffer.
   /// Returns -1 if the buffer is empty.
   public getStartIndex = () => {
-    if (this.length === 0) {
+    if (this._length === 0) {
       return -1;
     }
-    return Math.max(this.length - this.elements.length, 0);
+    return Math.max(this._length - this.elements.length, 0);
   };
 
   /// Add a new element to the buffer, potentially overriding old values.
   /// @param element The element to add
   public push = (element: T) => {
-    this.elements[this.length % this.elements.length] = element;
-    this.length++;
+    this.elements[this._length % this.elements.length] = element;
+    this._length++;
   };
 
   public get = (index: number) => {
     if (
-      this.length === 0 ||
+      this._length === 0 ||
       index < this.getStartIndex() ||
-      index >= this.length
+      index >= this._length
     ) {
       throw new RangeError(`The requested index ${index} is out of bounds`);
     }
@@ -33,7 +33,7 @@ export class CircularBuffer<T> {
 
   public getCopy = () => {
     let result = new CircularBuffer<T>(this.elements.length);
-    result.length = this.length;
+    result._length = this._length;
 
     for (let i = 0; i < this.elements.length; ++i) {
       result.elements[i] = this.elements[i];
@@ -42,11 +42,25 @@ export class CircularBuffer<T> {
     return result;
   };
 
-  constructor(capacity: number) {
+  public get length() {
+    return this._length;
+  }
+
+  constructor(capacity: number, initialValues: T[] = []) {
     if (capacity <= 0) {
       throw new RangeError("The buffer capacity must be > 0");
     }
+    if (initialValues.length > capacity) {
+      throw new RangeError(
+        `Cannot initialize buffer of size ${capacity} with ${initialValues.length} values.`,
+      );
+    }
+
     this.elements = new Array<T>(capacity);
-    this.length = 0;
+    this._length = 0;
+
+    for (const value of initialValues) {
+      this.push(value);
+    }
   }
 }
