8ebdf95da0dd26a21c2c5108fd4b779968ee07bc
Alessio Giuliano <alessio.giuliano@proton.me>
2025-08-18 11:16:48 +0200

    Disable undo / redo buttons when they cannot be used

--- frontend/src/components/editor/toolbar/actionButton.tsx
+++ frontend/src/components/editor/toolbar/actionButton.tsx
@@ -4,13 +4,20 @@ export interface ActionButtonProps {
   icon: ReactNode;
   onClick: () => void;
   style: string;
+  disabled: bool;
 }
 
-export const ActionButton = ({ icon, onClick, style }: ActionButtonProps) => {
+export const ActionButton = ({
+  icon,
+  onClick,
+  style,
+  disabled,
+}: ActionButtonProps) => {
   return (
     <button
-      className={`rounded-xl border-2 p-2 cursor-pointer ${style}`}
+      className={`rounded-xl border-2 p-2 ${disabled ? "" : "cursor-pointer"} ${style}`}
       onClick={() => onClick()}
+      disabled={disabled}
     >
       {icon}
     </button>
