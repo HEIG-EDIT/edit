1d94add422bf473d91fa24f0dc594d29cb600595
Alessio Giuliano <alessio.giuliano@proton.me>
2025-08-18 11:16:48 +0200

    Disable undo / redo buttons when they cannot be used

--- frontend/src/components/editor/toolbar/undoRedoSelector.tsx
+++ frontend/src/components/editor/toolbar/undoRedoSelector.tsx
@@ -1,6 +1,7 @@
 import { ActionButton } from "./actionButton";
 import UndoRoundedIcon from "@mui/icons-material/UndoRounded";
 import RedoRoundedIcon from "@mui/icons-material/RedoRounded";
+import colors from "tailwindcss/colors";
 
 interface UndoRedoSelectorProps {
   undo: () => void;
@@ -9,19 +10,33 @@ interface UndoRedoSelectorProps {
   canRedo: boolean;
 }
 
-export const UndoRedoSelector = ({ undo, redo }: UndoRedoSelectorProps) => {
+export const UndoRedoSelector = ({
+  undo,
+  redo,
+  canUndo,
+  canRedo,
+}: UndoRedoSelectorProps) => {
+  const getStyle = (canUse: boolean) =>
+    `${
+      canUse ? "bg-gray-900 border-violet-500" : "bg-gray-500 border-gray-900"
+    }`;
+
+  const getIconColor = (canUse: boolean) =>
+    canUse ? colors.violet[50] : colors.gray[400];
+
   return (
     <div className="flex gap-4">
-      {/* TODO: Disable buttons when cannot undo or redo */}
       <ActionButton
-        icon={<UndoRoundedIcon style={{ color: "white" }} />}
+        icon={<UndoRoundedIcon style={{ color: getIconColor(canUndo) }} />}
         onClick={undo}
-        style="bg-gray-900 border-violet-500"
+        style={getStyle(canUndo)}
+        disabled={!canUndo}
       />
       <ActionButton
-        icon={<RedoRoundedIcon style={{ color: "white" }} />}
+        icon={<RedoRoundedIcon style={{ color: getIconColor(canRedo) }} />}
         onClick={redo}
-        style="bg-gray-900 border-violet-500"
+        style={getStyle(canRedo)}
+        disabled={!canRedo}
       />
     </div>
   );
