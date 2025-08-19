692cfac177e0929b3e4108cffac29f76149a118f
Diana Laurenti <diana.laurenti@heig-vd.ch>
2025-08-19 15:20:47 +0200

    fixed app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './users/users.module';
+import { UserModule } from './user/user.module';
 import { StorageModule } from './storage/storage.module';
+//import { ConfigModule } from './config/config.module';
 import { TokensService } from './tokens/tokens.service';
 import { EmailService } from './email/email.service';
 
