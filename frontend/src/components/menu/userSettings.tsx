8525bf7f946f2984a7090e7bf5f5907da1c650d7
Saskya Panchaud <saskya.panchaud@heig-vd.ch>
2025-08-22 09:54:15 +0200

    [feature] Update menu

--- frontend/src/components/menu/userSettings.tsx
+++ frontend/src/components/menu/userSettings.tsx
@@ -1,5 +1,5 @@
 export const UserSettings = () => {
-  // TODO : affichage dans user ?
+  // TODO : afficher infos du user (username, mdp et email) et possibilite de les modifier
   return (
     <div>
       <p className="text-violet-50 font-bold text-xl">User</p>
