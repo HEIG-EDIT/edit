43566f365ea7522e9423a8c71a42a0702605874c
Saskya Panchaud <saskya.panchaud@heig-vd.ch>
2025-08-22 08:49:36 +0200

    [feature] Landing page = root and redirect to "/projects" to start editing

--- frontend/src/components/landing_page/startButton.tsx
+++ frontend/src/components/landing_page/startButton.tsx
@@ -7,7 +7,8 @@ export const StartButton = () => {
   return (
     <button
       className="bg-violet-500 hover:cursor-pointer hover:bg-violet-400  text-violet-50 text-xl font-extrabold rounded-full px-4 py-2 border-3  border-violet-50"
-      onClick={() => router.push("/")}
+      // TODO : @Elbu -> gerer redirect selon credentials ici ?
+      onClick={() => router.push("/projects")}
     >
       Start EDITing
     </button>
