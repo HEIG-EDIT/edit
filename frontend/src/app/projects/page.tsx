fe13b1e21c0bc7bfca57b3d6158275241ac57170
Alessio Giuliano <alessio.giuliano@proton.me>
2025-08-25 13:59:48 +0200

    Add initial UI for project creation

--- frontend/src/app/projects/page.tsx
+++ frontend/src/app/projects/page.tsx
@@ -6,9 +6,79 @@ import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import { Menu } from "@/components/menu/menu";
 import { useState } from "react";
+import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import { useRouter } from "next/navigation";
+import { Vector2d } from "konva/lib/types";
 
 export default function ProjectSelection() {
   const [menuDisplay, setMenuDisplay] = useState<boolean>(false);
+
+  const [isNewProjectDisplayed, setIsNewProjectDisplayed] = useState(false);
+  const [projectSize, setProjectSize] = useState<Vector2d>();
+
+  const router = useRouter();
+
+  const projectCreationButton = (
+    <ConfigurationButton
+      icon={AddRoundedIcon}
+      onClick={() => setIsNewProjectDisplayed(true)}
+    />
+  );
+
+  const handleProjectCreation = () => {
+    // TODO pass correct size
+    router.push("editor");
+  };
+
+  const inputClassName = "w-24 rounded-md text-gray-900 bg-violet-200";
+
+  const projectCreationPopup = (
+    <div className="flex flex-row border-2 border-violet-300 p-2 bg-gray-500 rounded-xl">
+      <div className="flex flex-col px-4 gap-4 justify-center text-violet-50">
+        <div className="flex flex-row gap-4">
+          <label htmlFor="width">Width: </label>
+          <input
+            className={inputClassName}
+            name="width"
+            id="width"
+            type="number"
+            min="1"
+            defaultValue="800"
+            onChange={(e) => {
+              setProjectSize({ ...projectSize, x: Number(e.target.value) });
+            }}
+          />
+        </div>
+
+        <div className="flex flex-row gap-4">
+          <label htmlFor="height">Height: </label>
+          <input
+            className={inputClassName}
+            name="height"
+            id="height"
+            type="number"
+            min="1"
+            defaultValue="800"
+            onChange={(e) => {
+              setProjectSize({ ...projectSize, y: Number(e.target.value) });
+            }}
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-row gap-2">
+        <ConfigurationButton
+          icon={CheckRoundedIcon}
+          onClick={handleProjectCreation}
+        />
+        <ConfigurationButton
+          icon={CloseRoundedIcon}
+          onClick={() => setIsNewProjectDisplayed(false)}
+        />
+      </div>
+    </div>
+  );
 
   return (
     <main className="bg-gray-900 min-h-screen p-6">
@@ -16,8 +86,10 @@ export default function ProjectSelection() {
         <div className="bg-gray-600 flex flex-row rounded-xl p-4">
           <div className="w-1/4 flex flex-row items-center justify-start gap-12">
             <p className="text-violet-50 font-bold text-xl">Projects</p>
-            {/* TODO : gerer logique pour creer un nouveau projet */}
-            <ConfigurationButton icon={AddRoundedIcon} onClick={() => {}} />
+            {isNewProjectDisplayed
+              ? projectCreationPopup
+              : projectCreationButton}
+            <div className="text-violet-50 w-1/4"></div>
           </div>
           <div className="w-1/2"></div>
           <div className="w-1/4 flex flex-row items-center justify-start gap-12">
