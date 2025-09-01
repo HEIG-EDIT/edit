ba4c81807dfa8596c0aa868cbdabf9ee906de54c
Alessio Giuliano <alessio.giuliano@proton.me>
2025-09-01 14:02:50 +0200

    Add documentation to layer.ts

--- frontend/src/models/editor/layers/layer.ts
+++ frontend/src/models/editor/layers/layer.ts
@@ -16,6 +16,7 @@ import {
 
 export type LayerId = string;
 
+/// The attributes that are saved on disk for the JSON export of projects.
 export interface SerializedLayer {
   id: LayerId;
   name: string;
@@ -28,6 +29,8 @@ export interface SerializedLayer {
   size: Vector2d;
 }
 
+/// The configuration of the filters applied to the layer. These are set
+/// by the "adjust" tool
 export class FiltersConfig {
   gaussianBlur: GaussianBlurConfiguration = GaussianBlur.initialConfiguration;
   colorAndTone: ColorAndToneConfiguration = ColorAndTone.initialConfiguration;
@@ -38,35 +41,53 @@ export class FiltersConfig {
 export class Layer {
   name: string;
   id: LayerId; // UUID
+  /// Attribute allowing pixel-level manipulation of images
   imageData: ImageData | null = null;
   image: HTMLImageElement;
+  /// Whether or not the layer is selected, i.e. whether or not modifications
+  /// applided by tools are applied to the layer.
   isSelected: boolean = false;
 
-  // Refs to the underlying Konva components
+  /// Ref to the underlying Konva component, enables manipulation through the
+  /// "regular" Konva API instead of the "react-konva" API.
   groupRef: RefObject<Konva.Group | null>;
 
+  /// If false, the layer is hidden on the canvas
   isVisible: boolean = true;
+  /// The current position of the layer relative to the canvas, (0, 0) represents the top left.
   position: Vector2d = {
     x: 0,
     y: 0,
   };
+  /// The position of the layer relative to the canvas at the start of a dragging operation
+  /// (i.e. moving the layer on the canvas)
   positionBeforeDrag: Vector2d = {
     x: 0,
     y: 0,
   };
+  /// The scale of the layer on both axis, used when resizing the layers
   scale: Vector2d = {
     x: 1,
     y: 1,
   };
+  /// The rotation in degrees of the layer
   rotation: number = 0;
-  lines: Array<Line> = []; // Lines drawn free-hand on a Layer
+
+  /// The lines drawn free-hand with the paint brush tool, or the eraser tool
+  lines: Array<Line> = [];
+  /// The filters applied to the layer with the adjust tool.
   filters: Array<Filter> = [];
   filtersConfig: FiltersConfig = new FiltersConfig();
 
+  /// The size of the layer's image
   size: Vector2d;
 
+  /// Counter to give unique names to empty layers
   static emptyLayerCounter: number = 0;
 
+  /// Deep copy of a layer, used with the duplication button. Note that this funciton
+  /// gives the resulting layer a new ID which is mandatory as layers cannot have
+  /// duplicated IDs.
   static duplicate(other: Layer): Layer {
     const result = new Layer(
       other.name,
@@ -83,6 +104,7 @@ export class Layer {
     return result;
   }
 
+  /// Create a Layer from one read from a serailized JSON project.
   static async fromSerialized(serialized: SerializedLayer): Promise<Layer> {
     const {
       id,
@@ -117,6 +139,11 @@ export class Layer {
     });
   }
 
+  /// Constructor used to add new layers.
+  /// @param name The name of the layer, used only when creating from an image
+  /// @param image The initial layer image read from the user's disk
+  /// @param width The width of a new empty layer
+  /// @param height The height of a new empty layer
   constructor(
     name: string | null = null,
     image: HTMLImageElement | null = null,
@@ -153,4 +180,6 @@ export class Layer {
   }
 }
 
+/// Callback type used for layer updates within React, usually taking the previous
+/// layer object as an argument and returning the updated layer.
 export type LayerUpdateCallback = (layer: Layer) => Layer;
