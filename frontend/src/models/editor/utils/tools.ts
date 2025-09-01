2e95898ff06ec5766993197fd3c8d2019c76d6b3
Alessio Giuliano <alessio.giuliano@proton.me>
2025-09-01 11:06:42 +0200

    Remove unimplemented tools

--- frontend/src/models/editor/utils/tools.ts
+++ frontend/src/models/editor/utils/tools.ts
@@ -1,5 +1,4 @@
 import { MOVE_TOOL } from "@/components/editor/tools/move";
-import { CROP_TOOL } from "@/components/editor/tools/crop";
 import { PAINT_TOOL } from "@/components/editor/tools/paint";
 import { ERASE_TOOL } from "@/components/editor/tools/erase";
 import { ADJUST_TOOL } from "@/components/editor/tools/adjust";
@@ -9,7 +8,6 @@ import { ToolConfiguration } from "../tools/toolConfiguration";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const TOOLS: Record<string, Tool<any>> = {
   [MOVE_TOOL.name]: MOVE_TOOL,
-  [CROP_TOOL.name]: CROP_TOOL,
   [PAINT_TOOL.name]: PAINT_TOOL,
   [ERASE_TOOL.name]: ERASE_TOOL,
   [ADJUST_TOOL.name]: ADJUST_TOOL,
