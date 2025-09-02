4eba58254b27782e9437a8ded2b857234259f66c
Elbunita <101522406+KurohanaNita@users.noreply.github.com>
2025-09-02 19:57:05 +0200

    Auth google debug test

--- backend/src/auth/strategies/google.strategy.ts
+++ backend/src/auth/strategies/google.strategy.ts
@@ -14,7 +14,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL as string,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL_PROD as string,
       scope: ['email', 'profile'],
     });
   }
